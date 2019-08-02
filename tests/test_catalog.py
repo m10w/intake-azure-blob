be0f2ff3de15ed1276c83af28e9429737b71e95b
hamed <hamed2005@gmail.com>
2019-08-02 14:17:24 +0200

    Remove unnecessary patched method

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -15,7 +15,7 @@ def local_prefix_cat():
 
 
 def test_local_prefix_catalog(local_prefix_cat):
-    source = local_prefix_cat['azure_blob'].get()
+    source = local_prefix_cat['test_azure_blob'].get()
     ds = source.read()
     assert isinstance(ds, pd.DataFrame)
     assert len(ds) == 622007
@@ -34,14 +34,13 @@ def test_azure_prefix_catalog():
     def opener(url, mode='r'):
         return open(_azure_uri_to_local(url), mode)
 
-    with patch('s3fs.S3FileSystem') as mockopen, patch('dask.dataframe.read_csv') as mock_read_csv:
+    with patch('dask.dataframe.read_csv') as mock_read_csv:
         # patch('intake_s3_manifests.s3_manifest') as s3_manifest,
 
-        mockopen.return_value.open.side_effect = opener
         mock_read_csv.side_effect = read_csv
         path = os.path.dirname(__file__)
-        azure_prefix_cat = open_catalog(os.path.join(path, 'data', 'test.csv'))
-        source = azure_prefix_cat['azure_blob'].get()
+        azure_prefix_cat = open_catalog(os.path.join(path, 'data', 'azure.catalog.yaml'))
+        source = azure_prefix_cat['test_azure_blob'].get()
         ds = source.read()
         assert isinstance(ds, pd.DataFrame)
         assert len(ds) == 622007
